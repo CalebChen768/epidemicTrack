489570d424754727827c645806e3d1e1638d9774
CalebChen768 <72332844+CalebChen768@users.noreply.github.com>
2025-02-21 10:33:41 +0100

    feat: db init and docker-compose for db

---
+++ init_db.py
@@ -0,0 +1,66 @@
+import pymysql
+from dotenv import load_dotenv
+import os
+
+# load environment variables
+load_dotenv()
+
+# MySQL configuration
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_PORT = os.getenv("MYSQL_PORT")
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_DB = os.getenv("MYSQL_DB")
+
+# initialize MySQL
+def init_mysql():
+    connection = pymysql.connect(host=MYSQL_HOST,
+                                port=int(MYSQL_PORT),
+                                 user=MYSQL_USER,
+                                 password=MYSQL_PASSWORD,
+                                 database=MYSQL_DB)
+    cursor = connection.cursor()
+
+    # create location table: location_id
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS checkpoints (
+        checkpoint_id VARCHAR(10) PRIMARY KEY
+    );
+    """)
+
+    # create transmission table: source_checkpoint_id, source_time, target_checkpoint_id, target_time
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS transmissions (
+        source_checkpoint_id VARCHAR(10),
+        source_time INT,
+        target_checkpoint_id VARCHAR(10) NOT NULL,
+        target_time INT NOT NULL,
+        PRIMARY KEY (source_checkpoint_id, source_time),
+        FOREIGN KEY (source_checkpoint_id) REFERENCES checkpoints(checkpoint_id) ON DELETE CASCADE,
+        FOREIGN KEY (target_checkpoint_id) REFERENCES checkpoints(checkpoint_id) ON DELETE CASCADE
+    );
+    """)
+    
+    # create risk table: checkpoint_id, time, risk
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS risks (
+        checkpoint_id VARCHAR(10),
+        time INT,
+        risk ENUM('medium', 'high') NOT NULL,
+        PRIMARY KEY (checkpoint_id, time),
+        FOREIGN KEY (checkpoint_id) REFERENCES checkpoints(checkpoint_id) ON DELETE CASCADE
+    );
+    """)
+
+    connection.commit()
+
+    print("""
+    --------------------------------
+    Checkpoints database initialized
+    --------------------------------
+    """)
+    connection.close()
+
+
+if __name__ == "__main__":
+    init_mysql()
